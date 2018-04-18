7f9e68d493bbf6d52933982c7132cba2cf440764
Nagendra Mutyala <mnagendra@pkrmsezdc.com>
2018-04-18 18:25:54 +0530

    Changed BodyParser

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
