f13a7bbcc059dc0d01a8e7677b7f5a143b7cc8b1
Nagendra Mutyala <mnagendra@pkrmsezdc.com>
2018-04-18 18:29:23 +0530

    Changed config file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,15 @@
 var env = process.env.NODE_ENV || 'development';
 
+console.log('----------> ENV Value :: ', env)
+
 if (env === 'development') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}else if(env === 'PROD'){
   var config = require('./config.json');
   var envConfig = config[env];
 
